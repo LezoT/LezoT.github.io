d25a3f95f7846830502d70984fe90c200e8ec92d
M.S.4 <klipovici@gmail.com>
2021-01-06 18:38:47 +0200

    .

--- assets/js/scrolltotopbtn.js
+++
@@ -1,8 +0,0 @@
-// Check to see if the window is top if not then display button
-$(window).scroll(function () {
-    if ($(this).scrollTop() > 400) {
-        $('.scrolltotop').fadeIn();
-    } else {
-        $('.scrolltotop').fadeOut();
-    }
-});
